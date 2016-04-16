21664d03b7946241c9b92f7097ee839cc39ea930
Bryan Yee <yee.bryan.c@gmail.com>
2016-04-16 17:31:54 +0000

    Completed Recursion exercise

--- recursion.js
+++ recursion.js
@@ -1,5 +1,14 @@
-function reduce(arr, fn, initial) {
-      // SOLUTION GOES HERE
-    }
+function reduce(arr, fn, initial) {       
+      
+      //base condition
+      if (arr.length === 0) return initial;
+
+      //recursive calls
+      var nextArray = arr.slice(1);
+      initial = fn(initial, arr[0]);
+      return reduce(nextArray, fn, initial);
+}
     
+    
+   
     module.exports = reduce
